4fb0db8c80d08fca0c8357d59117cf047e44e171
Marius Gundersen <marius@gundersen.xyz>
2017-05-28 18:48:38 +0200

    Implemented clientId

--- test/index.js
+++ test/index.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
 var oslobysykkel = require('../index');
+var clientId = '647bcbaf0a148724bab0a654e9215461'
 
 describe('getStations', function() {
   it('well played', function(done) {
-    oslobysykkel.getStations(function(result) {
+    oslobysykkel.getStations(clientId, function(result) {
       if (result && result !== undefined) {
         if (result.result !== undefined && result.error == 0) {
           done();
@@ -15,7 +16,7 @@ describe('getStations', function() {
 
 describe('getAvailability', function() {
   it('well played', function(done) {
-    oslobysykkel.getAvailability(function(result) {
+    oslobysykkel.getAvailability(clientId, function(result) {
       if (result && result !== undefined) {
         if (result.result !== undefined && result.error == 0) {
           done();
